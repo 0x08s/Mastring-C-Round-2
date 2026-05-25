c8349da3b66eaf353fcd8e639631961831a40a08
sawera <241689067+echomesawera@users.noreply.github.com>
2026-05-25 21:19:25 +0500

    Initial commit: Midterm preparation codes

---
+++ Practice.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int swap(int*,int*){
+
+
+
+    
+}
+
+int main(){
+
+
+return 0; 
+
+}
---
+++ Untitled1.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+usingnamespace std;
+
+int main (){
+
+string name = "sawera";
+
+cout <<name<<endl;
+
+string id   = 'BC250438279';
+
+cout <<id<<endl;
+
+return 0;
+}
