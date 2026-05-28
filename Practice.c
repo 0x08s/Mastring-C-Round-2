c58cd4a3460b326090f890da67af26dae81277b1
sawera <241689067+echomesawera@users.noreply.github.com>
2026-05-28 13:23:09 +0500

    Initial commit: Midterm preparation codes

--- Practice.c
+++ Practice.c
@@ -1,16 +1,37 @@
 #include<stdio.h>
-#include<math.h>
 
-int swap(int*,int*){
+#include <math.h>
 
+int main (){
 
+int n;
+printf("enter your number = ");
+scanf("%d",&n);
 
-    
+for(int i = 1; i <= n ;i++){
+
+if(i % 2 != 0 ){
+
+for(int j = 1;j<= i;j++){    
+
+printf("%d",j);
+
+}
 }
 
-int main(){
+else {
 
+for(int j = 1; j<= i;j++){
 
-return 0; 
+printf("%c",j+64);
 
+}
+
+}
+
+printf("\n");
+
+}
+
+return 0;    
 }
---
+++ practice1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+
+
+
+
+int main(){
+
+//int n;
+//printf("enter your number = ");
+//scanf("%d",&n);
+
+
+
+
+return 0;    
+}
